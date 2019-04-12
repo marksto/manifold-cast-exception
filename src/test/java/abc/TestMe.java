9bb7a73b8bac0ddc80ea038fa4d39ab45343b1b7
Mark Sto <mstoyukhin@gmail.com>
2019-04-12 05:39:44 +0300

    initial commit

--- src/test/java/abc/TestMe.java
+++ src/test/java/abc/TestMe.java
@@ -2,8 +2,10 @@ package abc;
 
 import junit.framework.TestCase;
 
+import java.net.URISyntaxException;
+
 public class TestMe extends TestCase {
-  public void testMe() {
+  public void testMe() throws URISyntaxException {
     abc.RunMe.main(null);
   }
 }
